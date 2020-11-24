9bd65b3a99f7e8a4be9ffd822f39d710ab58681f
isac hellström <63723578+isachells92@users.noreply.github.com>
2020-11-24 16:17:12 +0100

    Add files via upload
    Ado.net Api for connection to relational databases using c#

---
+++ ADOnetDemo/ADOnetDemo/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADOnetDemo
+{
+    class Employee
+    {
+        public string Namn { get; set; }
+        public decimal Lön { get; set; }
+        public string Titel { get; set; }
+
+        // public override e bra skit............
+        public override string ToString()
+        {
+            return $"Namn: {Namn} Lön: {Lön} Titel: {Titel}";
+        }
+    }
+}
---
+++ ADOnetDemo/ADOnetDemo/Models/Fruit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ADOnetDemo.Models
+{
+    class Fruit
+    {
+
+        public int ID { get; set; }
+        public string FruitType { get; set; }
+        public string FruitName { get; set; }
+        public decimal? PricePerKg { get; set; } // här accepterar decimal nullvärden med "?"
+
+        public override string ToString()
+        {
+            return $" ID: {ID} Frukttyp: {FruitType} Fruktnamn: {FruitName} Pris per Kilo: {PricePerKg}";
+        }
+    }
+}
